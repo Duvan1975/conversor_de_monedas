e73910d2f79bb85c7a85e53ee6046ac75aa826c2
Duvan Ballesteros <ballexplorer1975@gmail.com>
2024-10-18 20:53:10 -0500

    Solicitando datos al usuario tanto de tipo de moneda como la cantidad

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -11,26 +11,45 @@
             //Crear un objeto scanner para capturar la entrada del usuario
             Scanner teclado = new Scanner(System.in);
 
-            //Pedir al usuario que ingrese la cantidad de dólares USD
-            System.out.println("Digite la cantidad de dólares");
-            double cantidadUSD = teclado.nextDouble();
+            //Pedir al usuario que ingrese el tipo de moneda
+            System.out.println("Digite el tipo de moneda: USD, COP, ARS");
+            String monedaBase = teclado.nextLine().toUpperCase();
 
-            ConsultaMoneda consulta = new ConsultaMoneda("USD", cantidadUSD);
-            Moneda moneda = consulta.conversorDeMoneda("USD");
+            //Pedir al usuario que ingrese la cantidad de dinero
+            System.out.println("Digite la cantidad de: " + monedaBase);
+            double cantidad = teclado.nextDouble();
+
+            ConsultaMoneda consulta = new ConsultaMoneda(monedaBase, cantidad);
+            Moneda moneda = consulta.conversorDeMoneda(monedaBase);
             //System.out.println(moneda);
 
-            //Obtenemos la tasa de cambio de COP
-            Map<String, Double> conversionRates = moneda.getConversion_rates();
-            double tasaCOP = conversionRates.get("COP");
-            double tasaARS = conversionRates.get("ARS");
-            System.out.println("Peso Argentino " + tasaARS);
+            //Pedir al usuario que ingrese el tipo de moneda convertir
+            System.out.println("Digite el tipo de moneda: USD, COP, ARS a convertir");
+            teclado.nextLine();
+            String monedaDestino = teclado.nextLine().toUpperCase();
 
-            //Realizar la conversión
+            //Obtenemos la tasa de cambio
+            Map<String, Double> conversionRates = moneda.getConversion_rates();
+            Double tasaDestino = conversionRates.get(monedaDestino);
+
+            if (tasaDestino != null){
+                //Realizar la conversión
+                double cantidadConvertida = cantidad * tasaDestino;
+                System.out.println(String.format("%.2f %s equivalen a: %.2f %s",
+                        cantidad, monedaBase, cantidadConvertida, monedaDestino));
+            } else {
+                System.out.println("Lo siento, el tipo de moneda no fue encontrado");
+            }
+
+            /*double tasaARS = conversionRates.get("ARS");
+            System.out.println("Peso Argentino " + tasaARS);
+            Realizar la conversión
             double cantidadEnCOP = (cantidadUSD * tasaCOP);
-            //Ejemplo para obtener el valor de COP
-            //double tasaUSD = conversionRates.get("USD");
+            Ejemplo para obtener el valor de COP
+            double tasaUSD = conversionRates.get("USD");
 
             System.out.println(String.format("%.1f USD dólares, equivalen a: $%.2f pesos colombianos",
-                    cantidadUSD, cantidadEnCOP));
+                    cantidadUSD, cantidadEnCOP));*/
+
         }
     }
